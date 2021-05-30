78c12a14c879d7ceae07805c3eb1d3871704b68b
light <shresthaprakash75@gmail.com>
2021-05-30 12:57:52 +0545

    making input filtration modular : removing monolith code

--- auto-ams.js
+++ auto-ams.js
@@ -1,3 +1,5 @@
+const {in_out, userInput} = require('./user_input');
+
 const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,18 +10,7 @@ const email = process.env.EMAIL;
 const password = process.env.PASSWORD;
 
 // input filtration
-const in_put = process.argv[2].toLowerCase();
-var in_out = "";
-
-if(in_put == 'in'){
-    in_out = "In"
-}else if (in_put == 'out') {
-    in_out = "Out"
-}
-else{
-    console.log('Either type IN or OUT and TRY AGAIN')
-    return
-}
+userInput()
 
 // core engine
 async function run () {
